7fe95b19be0e76b8dbe4d7c0db3296ee31c9ea89
wukaiwei1 <2596862515@qq.com>
2022-08-02 12:17:01 +0800

    完成主页权限设置

--- src/permission.js
+++ src/permission.js
@@ -1,1 +1,32 @@
 // 权限控制
+import router from '@/router'
+import store from '@/store'
+
+// 白名单（不用登录也能访问）
+const whiteList = ['/login', '/404']
+// 路由全局前置守卫
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  //登录了
+  if (token) {
+    //如果访问的登录页
+    if (to.path === '/login') {
+      // 跳回主页
+      next('/')
+    } else {
+      // 放行
+      next()
+    }
+  } else {
+    //没有登录
+    const isCludes = whiteList.includes(to.path)
+    if (isCludes) {
+      // 如果访问的是白名单，放行
+      next()
+    } else {
+      // 不是白名单，则跳转登录页
+      next('/login')
+    }
+  }
+  next()
+})
--- src/router/index.js
+++ src/router/index.js
@@ -47,12 +47,14 @@ export const constantRoutes = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
   },
 
   {
@@ -120,13 +122,15 @@ export const constantRoutes = [
             children: [
               {
                 path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
                 meta: { title: 'Menu1-2-1' }
               },
               {
                 path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                component: () =>
+                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
                 meta: { title: 'Menu1-2-2' }
               }
@@ -164,13 +168,16 @@ export const constantRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
-
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
+// 路由实例对象
 const router = createRouter()
+
+
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
